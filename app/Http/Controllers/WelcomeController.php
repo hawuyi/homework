a03f07a2af7478165e7bb2e355f3d14bd40d0376
hawuyi <b123456789101115@gmail.com>
2023-10-23 02:03:34 +0800

    上傳

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function about()
+    {
+        $heroes = ["悟空", "達爾", "鳴人", "佐助", "魯夫"];
+        return view("pages/about", ['heroes'=> $heroes]);
+    }
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts/application')
+
+@section('main')
+    <h1>Hi</h1>
+
+    @if (count($heroes) > 0)
+        @foreach ($heroes as $hero)
+            @if($loop->first || $loop->last)
+                <li>
+                    <strong>{{ $hero }}</strong>
+                </li>      
+                
+            @else
+                <li>
+                    {{ $hero }}
+                </li>
+            @endif       
+        @endforeach   
+    @endif
+@endsection
